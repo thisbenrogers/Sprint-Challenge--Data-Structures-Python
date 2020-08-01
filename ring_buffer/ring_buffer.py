c3f224d1911e49921987443570d3e795ec7636b3
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-31 20:13:41 -0500

    Adds basic attributes to RingBuffer class, including __init__ and __repr__

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.cap = capacity
+        self.data = []
+
+    def __str__(self):
+        return f"{self.data}"
+
+    def __repr__(self):
+        return (f"ListNode("
+                f"\n\capacity={self.cap}"
+                f"\n\data={self.data}\n)")
 
     def append(self, item):
         pass
