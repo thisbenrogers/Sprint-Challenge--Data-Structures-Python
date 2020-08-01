381e1db9f7c896515c3c0d1e34ca8bb2c2d9f502
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-31 20:52:43 -0500

    Reformats in favor of simpler solution

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,29 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.data = []
+        self.current = 0
+        self.data = [None]*capacity
 
-    def __str__(self):
-        return f"{self.data}"
-
-    def __repr__(self):
-        return (f"ListNode("
-                f"\n\capacity={self.capacity}"
-                f"\n\data={self.data}\n)")
-
-    class __Full:
-        def append(self, item):
-            self.data[self.current] = item
-            self.current = (self.current + 1) % self.capacity
-        
-        def get(self):
-            return self.data[self.current:] + self.data[:self.current]
 
     def append(self, item):
-        self.data.append(item)
-        if len(self.data) == self.capacity:
+        self.data[self.current] = item
+        if self.current == self.capacity -1:
             self.current = 0
-            self.__class__ = self.__Full
+        else:
+            self.current += 1
 
     def get(self):
-        return self.data
+        ring = []
+        for i in self.data:
+            if i is not None:
+                ring.append(i)
+        return ring
