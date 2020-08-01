7644c9326a2198ef49c5f2b9546d1e086f6c57b3
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-31 21:20:35 -0500

    Implements two more performant structures

--- names/names.py
+++ names/names.py
@@ -10,13 +10,24 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# Fastest solution:
+duplicates = list(set(names_1).intersection(names_2))
+
+
+# Mid-level performance solution:
+# 
+# duplicates = [x for x in names_1 if x in names_2]
+
+
+# Original, not-performant iteration:
+# 
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
