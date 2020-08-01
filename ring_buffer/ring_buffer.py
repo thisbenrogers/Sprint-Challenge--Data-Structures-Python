0ea015b0706e43a404bd61002093de01fd7f6ba6
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-31 20:25:25 -0500

    Implements basic append() get() methods, passes 4 of 6 tests with 1 error

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -14,13 +14,16 @@ class RingBuffer:
     class __Full:
         def append(self, item):
             self.data[self.current] = item
-            self.current = (self.current + 1) % self.max
+            self.current = (self.current + 1) % self.cap
         
         def get(self):
             return self.data[self.current:] + self.data[:self.current]
 
     def append(self, item):
-        pass
+        self.data.append(item)
+        if len(self.data) == self.cap:
+            self.current = 0
+            self.__class__ = self.__Full
 
     def get(self):
-        pass
+        return self.data
